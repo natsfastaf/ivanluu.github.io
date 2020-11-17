59911d905819db46df3f3c64e897cfe856ca8169
ja0911 <ja0911@gmail.com>
2020-11-16 22:34:23 -0800

    commit

---
+++ src/pages/blog/Blog.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import {Container, Typography} from '@material-ui/core';
+
+
+const Blog: React.FC = () => {
+  return (
+    <div>
+      <Container maxWidth="md">
+        <Helmet>
+          <title>Projects</title>
+          <meta
+            name="description"
+            content="My latest interests"
+          />
+        </Helmet>
+        <Typography
+            variant="h2"
+            gutterBottom
+        >
+          Want to know what I've been interested in?
+        </Typography>
+      </Container>
+
+    </div>
+  );
+};
+
+export default Blog;
